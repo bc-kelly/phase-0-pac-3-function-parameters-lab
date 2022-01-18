2180a73afd00309fa68acc182520d1dee75f45fa
Bridget Kelly <bckelly197@gmail.com>
2022-01-18 13:58:55 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name = "Bridget") {
+    return (`Hi, my name is ${name}.`);
+}
+
+function introductionWithLanguage(name = "Bridget", language = "Python") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+function introductionWithLanguageOptional(name = "Bridget", language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
